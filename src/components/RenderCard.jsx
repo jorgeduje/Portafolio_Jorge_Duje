7065b6cdb0409fc9ef98d494bfbd8c2cdaa69037
jorgeduje <jorgeduje4@gmail.com>
2021-12-05 00:48:42 -0300

    work

--- src/components/RenderCard.jsx
+++ src/components/RenderCard.jsx
@@ -6,10 +6,10 @@ const RenderCard = ( {name, stage, description, technologies, url1, url2, image,
     return (
         <div className="card">
             <img src={image} alt="" />
-            <h3>{name}</h3>
-            <h5>{stage}</h5>
+            <h2>{name}</h2>
             <h4>{description}</h4>
             <h5>{technologies}</h5>
+            <h5>{stage}</h5>
             <div className="container-links">
                 <a href={url1}>Deploy</a>
                 <a href={url2}>github repositorie</a>
