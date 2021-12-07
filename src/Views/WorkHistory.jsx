4b736789d34b695053bb99137a4b0de96a0aed8d
jorgeduje <jorgeduje4@gmail.com>
2021-12-07 01:13:11 -0300

    my skills

--- src/Views/WorkHistory.jsx
+++ src/Views/WorkHistory.jsx
@@ -1,24 +1,32 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState} from 'react'
 import { Link } from 'react-router-dom'
 
 import "../Styles/WorkHistory.css"
 
 import ProfesionaInfoContext from "../Context/ProfesionalInfoContext"
 import RenderCard from '../components/RenderCard'
+import Loader from '../components/Loader'
 
 const WorkHistory = () => {
 
     const {Data} = useContext(ProfesionaInfoContext)
+    const [loader, setLoader] = useState(false)
+
+    setTimeout(() => {
+        setLoader(true)
+    }, 2000);
 
   
    
 
     return (
-        <div className="container-history">
+       loader ? 
+       <div className="container-history">
             <h2>Work History</h2>
 
             <div className="container-cards">
                 {
+                    
                     Data?.workHistory.map(( value )=>
                         <RenderCard 
                             key = {value.id}
@@ -36,8 +44,8 @@ const WorkHistory = () => {
             </div>
 
 
-            <Link to="/about" className="back"><i class="fas fa-arrow-left"></i></Link>
-        </div>
+        <Link to="/about" className="back"><i class="fas fa-arrow-left"></i></Link>
+        </div> : <Loader />
     )
 }
 
---
+++ src/components/Loader.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import "../Styles/Loader.css"
+
+const Loader = () => {
+    return (
+        <div className="container-loader">
+            <div className="background-loader">
+                <div className="loader-gray">
+                    <span className="loader-span">
+
+                    </span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Loader
