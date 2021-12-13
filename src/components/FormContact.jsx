d2532cc2f50f73ffce6ff78420d2b7da0e7fddde
jorgeduje <jorgeduje4@gmail.com>
2021-12-13 02:49:57 -0300

    otra cosa

--- src/components/FormContact.jsx
+++ src/components/FormContact.jsx
@@ -6,23 +6,71 @@ import "../Styles/FormContact.css"
 const FormContact = () => {
 
     const [ mailSend, setMailSend ] = useState(false)
+    const [ text, setText ]  = useState({name:"", email:"", phone:"", textarea:""})
+    const [ error, setError] = useState({displayError:""})
+    const [ isName, setIsName] = useState(false) 
+    const [ isEmail, setIsEmail] = useState(false) 
+    const [ isPhone, setIsPhone] = useState(false) 
+    const [ isText, setIsText] = useState(false) 
+
+
+
 
     const handlerDiv = ()=>{
         setMailSend(false)
     }
 
     const handlerMailSend = ( e )=>{
-        e.preventDefault()
-        emailjs.sendForm( "service_1jmny2t", "template_fa476kg", e.target, "user_r5y4dz9U8LPO02jOTjkPZ" )
-        .then( res =>{
-            console.log(res)
-        }).catch(err =>{
-            console.log(err)
-        })
 
-        e.target.reset()
-        setMailSend(true)
+        e.preventDefault()
+        if(text.name.length !== 0){
+
+            setIsName(true)
+            
+        }else{
+            setError({displayError:"Please, complete this area"})
+        }
+        if(text.email.length !== 0){
+
+            setIsEmail(true)
+            
+        }else{
+            setError({displayError:"Please, complete this area"})
+        }
+        if(text.phone.length !== 0){
+
+            setIsPhone(true)
+            
+        }else{
+            setError({displayError:"Please, complete this area"})
+        }
+        if(text.textarea.length !== 0){
+
+            setIsText(true)
+            
+        }else{
+            setError({displayError:"Please, complete this area"})
+        }
+        
+        if(  isName && isEmail && isPhone && isText){
+
+            emailjs.sendForm( "service_1jmny2t", "template_fa476kg", e.target, "user_r5y4dz9U8LPO02jOTjkPZ" )
+            .then( res =>{
+                console.log(res)
+            }).catch(err =>{
+                console.log(err)
+            })
+            setMailSend(true)
+
+            e.target.reset()
+
+            setError({displayError:""})
+        }
+
     }
+
+    let consultation = ` ${text.name} ${text.email} ${text.textarea} `
+    const whtasappLink = `https://api.whatsapp.com/send?phone=543544563586&text=${consultation}`
 
 
     return (
@@ -31,13 +79,13 @@ const FormContact = () => {
                 <h2>Send Me an Email</h2>
         
                     <form onSubmit={handlerMailSend}>
-                        <input type="text" placeholder='Name' name="name"/>
-                        <input type="text" placeholder='Email' name="email"/>
-                        <input type="text" placeholder='Phone' name="phone"/>
-                        <textarea name="" id="" cols="30" rows="10" name="message"></textarea>
+                        <input type="text" placeholder={error.displayError.length >0 ? error.displayError : 'Name'} name="name" onChange={(e)=> setText({...text, name: e.target.value})}/>
+                        <input type="email" placeholder={error.displayError.length >0 ? error.displayError : 'Email'} name="email" onChange={(e)=> setText({...text, email: e.target.value})}/>
+                        <input type="text" placeholder={error.displayError.length >0 ? error.displayError : 'Phone'} name="phone" onChange={(e)=> setText({...text, phone: e.target.value})}/>
+                        <textarea name="" id="" cols="30" rows="10" name="message" placeholder={error.displayError.length >0 ? error.displayError : 'Question:'} onChange={(e)=> setText({...text, textarea: e.target.value})}></textarea>
                         <div className='container-buttons'>
                             <button type='submit'>Email</button>
-                            <button>Whatsapp</button>
+                           <a href={whtasappLink} target="_blank" className="a">Whastapp</a>
                         </div>
                     </form> 
 
